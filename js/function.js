4a6ab55e6d9545633272173bf93a164dfdb6b8d3
闫增涛 <1829501689@qq.com>
2024-09-13 18:41:30 +0800

    apply函数

--- js/function.js
+++ js/function.js
@@ -1,4 +1,4 @@
-// 自定义call 函数
+// 1.自定义call函数
 // call(Fn,obj,...args)  接受三个参数，调用call函数将执行Fn函数，并将Fn运行内部将this指向obj对象，...args为Fn参数
 // 当obj未undefind或null，this指向全局对象
 function call(Fn,obj,...args) {
@@ -9,4 +9,16 @@ function call(Fn,obj,...args) {
   const reslut =  obj.temp(...args)
   delete obj.temp
   return reslut
+}
+
+// 2.自定义apply函数
+// apply函数同上call函数，区别是apply函数的第三个参数未一个数组
+function apply(Fn,obj,args) {
+  if(obj == undefined || obj == null) {
+    obj = globalThis
+  }
+   obj.temp = Fn
+   const result = obj.temp(...args)
+   delete obj.temp
+   return result
 }
